7f6208dba9715f6839c7aa013ac3ce22ac92a76e
Jesus De La Rosa <budderlod@gmail.com>
2024-10-06 15:47:09 -0400

    trustcontroller and img change

--- gamejam2024/Assets/Scripts/TrustController.cs
+++ gamejam2024/Assets/Scripts/TrustController.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TrustController : MonoBehaviour
 {
     [SerializeField] private GameObject player;
-    private int trustLevel;
+    [SerializeField] private GameObject daveEmotion;
+    [SerializeField] private Sprite daveHappy;
+    [SerializeField] private Sprite daveNeutral;
+    [SerializeField] private Sprite daveSad;
+
+    public int trustLevel;
+    private Image img;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        img = daveEmotion.GetComponent<Image>();
     }
 
     // Update is called once per frame
@@ -21,5 +28,15 @@ public class TrustController : MonoBehaviour
 
     public void trustAddRemove(int trust) {
         trustLevel += trust;
+
+        if(trustLevel >= 0) {
+            img.sprite = daveHappy;
+        }
+        if(trustLevel < 0 && trust >= -3) {
+            img.sprite = daveNeutral;
+        }
+        if(trustLevel < -3) {
+            img.sprite = daveSad;
+        }
     }
 }
